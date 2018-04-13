8931ce5719e7aa1fde7475a64d0bc3a6c83d1280
Bryan Castleman <castlemaninc@gmail.com>
2018-04-13 11:28:11 -0700

    installing packages in order to have gulp compile sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
-# day3-gulp-sass
+var gulp = require('gulp');
+var sass = require('gulp-sass');
 
+gulp.task('hello', function(){
+	console.log('Hello, world.');
+});
 
-This gulp task compiles Sass into CSS using middleware called gulp-sass.
-```
 gulp.task('sass', function(){
   return gulp.src('./scss/style.scss')
     .pipe(sass()) // Converts Sass to CSS with gulp-sass
     .pipe(gulp.dest('./css'))
 });
-
-```
